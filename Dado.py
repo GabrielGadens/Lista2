5be84a759c86a74a86d1afed9451f9cb29002dad
GabrielGadens <126627425+GabrielGadens@users.noreply.github.com>
2023-03-28 21:17:31 -0300

    Update Dado.py

--- Dado.py
+++ Dado.py
@@ -1,16 +1,21 @@
 import random
 class Dado:
-    def __init__(self, faces):
-        self.faces = faces
+    def __init__(self):
+        self.__faces = ''
     def rolar(self):
-        return random.randint(1,self.faces)
+        return random.randint(1,self.__faces)
+    def setFaces(self,faces):
+        self.__faces=faces
+    def getFaces(self):
+        return self.__faces
 i = True
+d1 = Dado()
 while (i):
     faces = int(input('Número de faces? 6, 8 ou 12\n'))
     if faces == 6 or faces == 8 or faces == 12 :
-        d1 = Dado(faces)
-        print(d1.rolar())
+        d1.setFaces(faces)
+        for i in range(0,3):
+            print(i+1,'º lance: ',d1.rolar())
         i=False
     else:
         print('Número inválido')
-
